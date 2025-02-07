76cb0b13049c6bd1f5b021c13a9f682725d61296
HariniUddagiri <157795052+HariniUddagiri@users.noreply.github.com>
2025-02-07 07:51:34 +0000

    add

---
+++ terraform-aws-ec2/ec2.tf
@@ -0,0 +1,27 @@
+resource "aws_instance" "ec2" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_all.id]
+  tags = var.ec2_tags
+}
+
+resource "aws_security_group" "sg" {
+  name ="allowall"
+  description = "moduleexplore"
+
+  ingress = {
+    from_port=22
+    to_port=22
+    protocol="tcp"
+    cidr_blocks="[0.0.0.0/0]"
+
+  }
+
+  egress = {
+    from_port=0
+    to_port=0
+    protocol=-"-1"
+    cidr_blocks=["0.0.0.0/0"]
+  }
+
+}
---
+++ terraform-aws-ec2/variables.tf
@@ -0,0 +1,16 @@
+variable "ami_id" {
+  default = "ami-09c813fb71547fc4f"
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "ec2_tags" {
+  default = {
+
+     Name    = "module"
+    Purpose = "explore"
+
+  }
+}
